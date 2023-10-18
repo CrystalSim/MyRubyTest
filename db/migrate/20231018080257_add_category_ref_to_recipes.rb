45538ea439efe2d7587c1cc94ea42875e89d3e0b
Simone <21373119@buaa.edu.cn>
2023-10-18 16:23:45 +0800

    fixed little

--- db/migrate/20231018080257_add_category_ref_to_recipes.rb
+++ db/migrate/20231018080257_add_category_ref_to_recipes.rb
@@ -1,5 +1,5 @@
 class AddCategoryRefToRecipes < ActiveRecord::Migration[7.1]
   def change
-    add_reference :recipes, :category,  foreign_key: true
+    add_reference :recipes, :category, null: false, foreign_key: true
   end
 end
